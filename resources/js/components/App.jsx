c5b9a56e1d654ef7d87d0b39acb8a8544ded5666
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-13 17:15:07 +0100

    Before build 1.

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 export default function App(){
     return(
         <div className="container text-center">
-            Hello React!
+            Hello React! Builded!
         </div>
     );
 };
