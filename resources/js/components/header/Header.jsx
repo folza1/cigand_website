a3d11c8e0d194fad91528197ec3c6ee4b040329d
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-17 18:18:50 +0100

    Iphone 7 1.

--- resources/js/components/header/Header.jsx
+++ resources/js/components/header/Header.jsx
@@ -4,6 +4,7 @@ export default function Header(){
     return(
         <div className='header'>
             <img src="/images/cigand_favicon.png" />
+            <p>Cigand</p>
         </div>
     );
 };
