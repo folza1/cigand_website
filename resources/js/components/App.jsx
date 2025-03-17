16618003ed3dedb8f7f2ff750b72c8daacb9b27b
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-17 19:55:42 +0100

    Slideshow 1.

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import Header from "./header/Header";
 import './app_component.css';
+import SlideShow from "./slideshow/SlideShow";
 export default function App(){
     return(
         <div id="app_component">
             < Header />
+            < SlideShow />
         </div>
     );
 };
---
+++ resources/js/components/slideshow/SlideShow.jsx
@@ -0,0 +1,8 @@
+import './slideshow.css';
+
+export default function SlideShow(){
+    return(
+        <div className='slideshow'>
+        </div>
+    );
+};
