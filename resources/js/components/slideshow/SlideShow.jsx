35d72aee35d6d48a41ab7e87fa9b6b7eff6c3087
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-17 20:06:43 +0100

    SlideShow text

--- resources/js/components/slideshow/SlideShow.jsx
+++ resources/js/components/slideshow/SlideShow.jsx
@@ -4,6 +4,8 @@ export default function SlideShow(){
     return(
         <div className='slideshow'>
             <p className='slideshow_text'>Cigánd Website!</p>
+            <p className='slideshow_text2'>Cigánd Website!</p>
+            <p className='slideshow_text3'>Cigánd Website!</p>
         </div>
     );
 };
