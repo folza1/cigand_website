6491dd7ffce1a9ebab0adbb6609d5f236cbdfdbd
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-19 16:01:04 +0100

    Slideshow Fade Done!

--- resources/js/components/slideshow/SlideShow.jsx
+++ resources/js/components/slideshow/SlideShow.jsx
@@ -1,18 +1,37 @@
 import './slideshow.css';
+import { useState, useEffect } from 'react';
 
 const SlideShow = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+  const images = [
+    "/images/slideshow/kep1.jpg",
+    "/images/slideshow/kep2.jpg",
+    "/images/slideshow/kep3.jpg"
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFade(false); // Indítja a fade-out animációt
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); // Képváltás
+        setFade(true); // Fade-in animáció
+      }, 1000); // 1 másodperc várakozás fade-out után
+    }, 3000); // 3 másodpercenként vált a kép
+
+    return () => clearInterval(intervalId); // Takarítás, ha a komponens eltűnik
+  }, []);
+
   return (
     <>
       <div className='slideshow'>
-        <div className='slideshow_container'>
-          <img src="/images/slideshow/kep1.jpg" />
+        <div className={`slideshow_container ${fade ? 'fade-in' : 'fade-out'}`}>
+          <img src={images[currentIndex]} alt="Slideshow" />
         </div>
       </div>
-      <p className='slideshow_text1'>Cigánd Website!</p>
-      <p className='slideshow_text2'>Cigánd Website!</p>
-      <p className='slideshow_text3'>Cigánd Website!</p>
     </>
   );
 };
 
 export default SlideShow;
+
