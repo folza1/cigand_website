0f61a71dbf1e4c689b4341aa55fa649719db748c
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-24 17:35:44 +0100

    Navbar Megamenu 1.

--- resources/js/components/navbar/Navbar.jsx
+++ resources/js/components/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import "./navbar.css";
-
 const Navbar = () => {
   return (
     <div className="navbar">
-      
+      <div className="dropbtn">Kezdőlap</div>
+      <div className="dropbtn">Hírek</div>
+      <div className="dropdown">
+        <div className="dropbtn">Városunk</div>
+        <div className="dropdown-content">Megamenu</div>
+      </div>
+      <div className="dropdown">
+        <div className="dropbtn">Önkormányzat</div>
+        <div className="dropdown-content">Megamenu</div>
+      </div>
+      <div className="dropdown">
+        <div className="dropbtn">Kultúra</div>
+        <div className="dropdown-content">Megamenu</div>
+      </div>
     </div>
   );
 };
