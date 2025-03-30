fb8788e01c228316b3219faef4f06b3b1a916966
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-03-30 19:06:23 +0200

    Close and Back button

--- resources/js/components/navbar/Navbar.jsx
+++ resources/js/components/navbar/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
         <div className={`mobile-menu ${isClosing ? "mobile-menu-closing" : ""}`}>
           {/* X gomb a menü bezárásához */}
           <div className="close-btn" onClick={toggleMobileMenu}>
-            X
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
           </div>
           <div className="mobile-menu-items">
             <div className="mobile-menu-item">
@@ -182,8 +182,10 @@ const Navbar = () => {
       {/* Sub-menük */}
       {isMobileSubMenu1Open && (
         <div className={`sub-menu1 ${isSubmenu1Closing ? "mobile-submenu-closing" : ""}`}>
-          <div className="close-btn" onClick={toggleMobileSubMenu1}>
-            X
+          <div className="back-btn" onClick={toggleMobileSubMenu1}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="left-arrow">
+              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
           </div>
           <div className="mobile-submenu-items">
             <div>Városunk 1</div>
@@ -193,10 +195,13 @@ const Navbar = () => {
         </div>
       )}
 
+
       {isMobileSubMenu2Open && (
         <div className={`sub-menu2 ${isSubmenu2Closing ? "mobile-submenu-closing" : ""}`}>
-          <div className="close-btn" onClick={toggleMobileSubMenu2}>
-            X
+          <div className="back-btn" onClick={toggleMobileSubMenu2}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="left-arrow">
+              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
           </div>
           <div className="mobile-submenu-items">
             <div>Önkormányzat 1</div>
@@ -208,8 +213,10 @@ const Navbar = () => {
 
       {isMobileSubMenu3Open && (
         <div className={`sub-menu3 ${isSubmenu3Closing ? "mobile-submenu-closing" : ""}`}>
-          <div className="close-btn" onClick={toggleMobileSubMenu3}>
-            X
+          <div className="back-btn" onClick={toggleMobileSubMenu3}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="left-arrow">
+              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
           </div>
           <div className="mobile-submenu-items">
             <div>Kultúra 1</div>
