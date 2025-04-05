8ea98e654882cff8c712281a75f1b94fb3f7710c
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-04-05 20:46:02 +0200

    Vite config changed!

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+
 
 export default defineConfig({
     plugins: [
@@ -9,5 +11,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        legacy({
+            targets: ['defaults'], // Megadhatod, hogy mely böngészőket támogasson
+          }),
     ],
 });
