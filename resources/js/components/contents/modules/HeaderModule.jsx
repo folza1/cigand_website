2beb5c1035221b51fac4f942730e851114df7369
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-05-01 20:26:08 +0200

    Container triangle is Done!

--- resources/js/components/contents/modules/HeaderModule.jsx
+++ resources/js/components/contents/modules/HeaderModule.jsx
@@ -2,14 +2,19 @@ import './header_module.css';
 
 export default function HeaderModule({ children }) {
     return (
-        <div className='header_of_content'>
-            <div className='heading_head'>{children}</div>
-            <div className='heading_line_container'>
-                <div className='heading_line'>
-                    <div className='heading_line2'></div>
+        <>
+            <div className='header_of_content'>
+                <div className='heading_head'>{children}</div>
+                <div className='heading_line_container'>
+                    <div className='heading_line'>
+                        <div className='heading_line2'></div>
+                    </div>
                 </div>
+                <div className='breadcrumb'><span className='breadcrumb_text1'>Városunk</span><span className='breadcrumb_slash'>/</span><span className='breadcrumb_text2'>Információk</span><span className='breadcrumb_slash'>/</span><span className='breadcrumb_text3'>{children}</span></div>
             </div>
-            <div className='breadcrumb'><span className='breadcrumb_text1'>Városunk</span><span className='breadcrumb_slash'>/</span><span className='breadcrumb_text2'>Információk</span><span className='breadcrumb_slash'>/</span><span className='breadcrumb_text3'>{children}</span></div>
-        </div>
+            <div className='header_triangle_container'>
+                <div className='header_triangle'></div> {/* Új háromszög elem */}
+            </div>
+        </>
     );
 };
