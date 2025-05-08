bfd56619af28a394d32090b0bff35a0f17da0c38
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-05-08 22:10:28 +0200

    Slideshow pic is set

--- resources/js/components/slideshow/SlideShow.jsx
+++ resources/js/components/slideshow/SlideShow.jsx
@@ -35,14 +35,18 @@ const SlideShow = () => {
           </div>
         </div>
         <div className='events_and_documents'>
-          <div className='documents'>Friss dokumentumok</div>
+          <div className='documents'>
+            <img src="/images/slideshow/videki_otthonfelujitasi_prg_522.jpg" alt="Friss esemény" />
+          </div>
           <div className='events'>
             <div className='event1'>Friss események1</div>
             <div className='event2'>Friss események2</div>
           </div>
         </div>
       </div>
-      <div className='documents_600'>Friss dokumentumok</div>
+      <div className='documents_600'>
+        <img src="/images/slideshow/videki_otthonfelujitasi_prg_522.jpg" alt="Friss esemény" />
+      </div>
       <div className='event1_600'>Friss események1</div>
       <div className='event2_600'>Friss események2</div>
     </>
