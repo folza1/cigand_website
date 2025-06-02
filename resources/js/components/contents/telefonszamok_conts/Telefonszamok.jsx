8a464db630558198ef3c2ef593668abdc0fd997f
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-06-02 19:41:26 +0200

    Telefonszamok is done!

--- resources/js/components/contents/telefonszamok_conts/Telefonszamok.jsx
+++ resources/js/components/contents/telefonszamok_conts/Telefonszamok.jsx
@@ -55,7 +55,7 @@ export default function Telefonszamok() {
                     <div className="telefonszamok_hatosagi_igazgatasi">
                         <div>
                             <p className="bold uppercase align-center">
-                                Hatósági és igazgatási osztály
+                                Hatósági és igazgatási Osztály
                             </p>
                         </div>
                         <hr />
@@ -72,6 +72,115 @@ export default function Telefonszamok() {
                         <div className="telefonszamok_element">
                             <p className="bold">Dócsné Tóth Evelin</p>
                             <p>47/534-417 mellék:1180</p>
+                        </div>
+                    </div>
+
+                    <div className="telefonszamok_varosfejlesztesi">
+                        <div>
+                            <p className="bold uppercase align-center">
+                                Városfejlesztési Osztály
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Gönczi Zsolt</p>
+                            <p>47/534-425 mellék:1120</p>
+                        </div>
+                    </div>
+
+                    <div className="telefonszamok_penzugyi">
+                        <div>
+                            <p className="bold uppercase align-center">
+                                Pénzügyi Osztály
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Illés Lászlóné</p>
+                            <p>47/534-405 mellék:1080</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Csákiné Barnóczki Márta</p>
+                            <p>47/534-405 mellék:1090</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Varga Lászlóné</p>
+                            <p>47/534-405 mellék:1090</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Göncziné Tóth Renáta</p>
+                            <p>47/534-405 mellék:1080</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Nagyné Fodor Anita</p>
+                            <p>47/534-405 mellék:1090</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Timári Dóra </p>
+                            <p>47/534-406 mellék:1100</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Illés Józsefné</p>
+                            <p>47/534-422 mellék:1110</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Csákiné Szilágyi Erika</p>
+                            <p>47/534-422 mellék:1110</p>
+                        </div>
+                    </div>
+
+                    <div className="telefonszamok_intezmenyek">
+                        <div>
+                            <p className="bold uppercase align-center">
+                                Intézmények
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Településüzemeltetés</p>
+                            <p>47/534-413 mellék:1130</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Óvoda</p>
+                            <p>47/534-415 mellék:1140</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Bölcsőde</p>
+                            <p>47/534-416 mellék:1170</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Művelődési Ház</p>
+                            <p>47/534-418 mellék:1160</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Könyvtár</p>
+                            <p>47/534-419 mellék:1150</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Savanyitóüzem</p>
+                            <p>20/423-2627</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Szabadidőközpont</p>
+                            <p>47/534-043</p>
+                        </div>
+                        <hr />
+                        <div className="telefonszamok_element">
+                            <p className="bold">Bútorüzem</p>
+                            <p>47/534-400</p>
                         </div>
                     </div>
                 </div>
