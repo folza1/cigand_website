4c25928d5763dc4fed64d368d83b292f0ab67399
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-07-01 18:18:14 +0200

    CJH in progress 4!

--- resources/js/components/contents/cigand_jarasi_hivatal_conts/CigandJarasiHivatal.jsx
+++ resources/js/components/contents/cigand_jarasi_hivatal_conts/CigandJarasiHivatal.jsx
@@ -342,6 +342,186 @@ export default function CigandJarasiHivatal() {
 
                         <p className="align-center">+36 47 795-097</p>
                     </div>
+                    <p className="bold align-center uppercase margin_top_cigand_jarasi_hivatal">
+                        A Járási Hivatalban intézhető hatósági ügyek
+                    </p>
+                    <p className="bold margin_top_cigand_jarasi_hivatal">
+                        Szociális ellátások:
+                    </p>
+                    <ul className="ul_1_cigand_jarasi_hivatal padding_CJH">
+                        <li>
+                            közgyógyellátásra jogosultsággal kapcsolatos ügyek
+                            az alábbi esetekben:
+                            <ul className="ul_2_cigand_jarasi_hivatal">
+                                <li>
+                                    átmeneti gondozott, az átmeneti és tartós
+                                    nevelésbe vett kiskorú;
+                                </li>
+                                <li>
+                                    rendszeres szociális segélyben részesülő
+                                    egészségkárosodott személy;
+                                </li>
+                                <li>
+                                    pénzellátásban részesülő hadigondozott és a
+                                    nemzeti gondozott;
+                                </li>
+                                <li>központi szociális segélyben részesülő;</li>
+                                <li>rokkantsági járadékos;</li>
+                                <li>
+                                    az, aki
+                                    <ul className="ul_3_cigand_jarasi_hivatal">
+                                        <li>
+                                            jogszabályban meghatározott
+                                            rokkantsági ellátásban részesül
+                                        </li>
+                                        <li>
+                                            rokkantsági ellátásban részesül, és
+                                            a közgyógyellátásra való
+                                            jogosultságát 2012. április 15-éig
+                                            megállapították,
+                                        </li>
+                                        <li>
+                                            öregségi nyugdíjban részesül és
+                                            2011. december 31-én I. vagy II.
+                                            csoportú rokkantsági, baleseti
+                                            rokkantsági nyugdíjra volt jogosult,
+                                            vagy
+                                        </li>
+                                        <li>
+                                            öregségi nyugdíjban részesül, és a
+                                            nyugdíjra való jogosultságának
+                                            megállapítását megelőző napon a i)
+                                            alpont hatálya alá tartozott;
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    aki, vagy aki után szülője vagy eltartója
+                                    magasabb összegű családi pótlékban részesül.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            ápolási díjra való jogosultsággal kapcsolatos ügyek
+                            azoknak, akik
+                        </li>
+                        <li>
+                            egészségügyi szolgáltatásra jogosultsághoz
+                            rászorultság igazolása
+                        </li>
+                        <li>időskorúak járadékával kapcsolatos ügyek</li>
+                        <li>hadigondozási ügyek</li>
+                        <li>
+                            foglalkoztatást helyettesítő támogatás,
+                            egészségkárosodási és gyermekfelügyeleti támogatás
+                        </li>
+                    </ul>
+
+                    <p className="bold margin_top_cigand_jarasi_hivatal">
+                        Egyéb hatósági ügyek:
+                    </p>
+                    <ul className="ul_1_cigand_jarasi_hivatal padding_CJH">
+                        <li>ingatlanfelügyeleti hatósági jogkör</li>
+                        <li>
+                            temetkezési szolgáltatás engedélyezése, temető
+                            fenntartásának, üzemeltetésének ellenőrzése
+                        </li>
+                        <li>
+                            cirkuszi menazséria engedélyezése, a szolgáltatás
+                            felügyelete
+                        </li>
+                        <li>
+                            állatpanzió és állatmenhely (állatotthon)
+                            engedélyezésével és a szolgáltatás felügyeletével
+                            kapcsolatos állatvédelmi hatósági ügyek
+                        </li>
+                        <li>szabálysértési ügyek</li>
+                    </ul>
+
+                    <p className="bold margin_top_cigand_jarasi_hivatal">
+                        Menekült, oltalmazott, menedékes személyekkel
+                        kapcsolatos feladatok:
+                    </p>
+
+                    <ul className="ul_1_cigand_jarasi_hivatal padding_CJH">
+                        <li>
+                            letelepedéshez nyújtott támogatás, rendszeres
+                            létfenntartási támogatás folyósítása
+                        </li>
+                        <li>
+                            lakáscélú támogatásával összefüggő hatáskörök úgy,
+                            mint
+                        </li>
+                        <li>a szerződés megkötése,</li>
+                        <li>jelzálogjog bejegyzésének kezdeményezése,</li>
+                        <li>támogatás folyósítása és visszatéríttetése</li>
+                    </ul>
+
+                    <p className="bold margin_top_cigand_jarasi_hivatal">
+                        Környezetvédelmi, vízügyi igazgatási feladatok:
+                    </p>
+                    <ul className="ul_1_cigand_jarasi_hivatal padding_CJH">
+                        <li>
+                            közüzemi szolgáltatók esetében a vételezési illetve
+                            mérőhelyre a szolgáltató bejutásával összefüggő
+                            feladatok
+                        </li>
+                        <li>
+                            közműves ivóvíz és szennyvízelvezető hálózat rákötés
+                            fogyasztó költségére, szabálytalan rákötés
+                            elbontására, átalakítására, hibák kijavítására
+                            kötelezés, szomszédos ingatlanon szolgalom
+                            alapítása,
+                        </li>
+                        <li>
+                            vízmérő közterületen lévő helyének megállapítása
+                        </li>
+                        <li>levegőtisztaság-védelmi hatósági ügyek</li>
+                    </ul>
+
+                    <p className="bold margin_top_cigand_jarasi_hivatal">
+                        Okmányokkal kapcsolatos ügyek:
+                    </p>
+
+                    <ul className="ul_1_cigand_jarasi_hivatal padding_CJH">
+                        <li>
+                            a jármű forgalomba helyezése vagy forgalomból való
+                            kivonása, forgalmi engedély cseréje
+                        </li>
+                        <li>a jármű adásvétele esetén a tulajdonjog átírása</li>
+                        <li>
+                            új forgalmi engedély, illetve törzskönyv kiadása
+                        </li>
+                        <li>
+                            a személyazonosító igazolvány kiadásával kapcsolatos
+                            ügyek és a lakcímkártya kiállítása
+                        </li>
+                        <li>
+                            útlevél kiadásával, cseréjével kapcsolatos ügyek
+                        </li>
+                        <li>
+                            egyéni vállalkozói tevékenység megkezdéshez
+                            szükséges regisztráció, szüneteltetésével, illetve
+                            megszüntetésével kapcsolatos ügyek
+                        </li>
+                        <li>
+                            mozgásában korlátozott személyek parkolási
+                            igazolványával kapcsolatos ügyek
+                        </li>
+                        <li>személyiadat- és lakcímnyilvántartás</li>
+                        <li>
+                            a lakóhely, vagy tartózkodási hely létesítése
+                            (lakcímbejelentési eljárás)
+                        </li>
+                        <li>
+                            a lakóhely, vagy tartózkodási hely fiktívvé
+                            nyilvánításával kapcsolatos eljárások
+                        </li>
+                        <li>
+                            letelepedett harmadik országbeli személy
+                            lakcímbejelentése
+                        </li>
+                    </ul>
                 </div>
             </ContentContainer>
         </>
