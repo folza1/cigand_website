effd6eb9b39c32361e08fb0006f8ecf07a07c4c9
Fodor Zoltán <fodorzoltan317@gmail.com>
2025-07-23 20:49:02 +0200

    TelepulesUzemeltetes in progress 3!

--- resources/js/components/contents/telepules_uzemeltetes_conts/TelepulesUzemeltetes.jsx
+++ resources/js/components/contents/telepules_uzemeltetes_conts/TelepulesUzemeltetes.jsx
@@ -13,22 +13,190 @@ export default function TelepulesUzemeltetes() {
             </HeaderModule>
             <ContentContainer>
                 <div className="telepules_uzemeltetes_container">
-                    <NewsHeader new_date="2015. június 30.">
-                        Tranzitfoglalkoztatás megvalósítása Cigánd városában
-                    </NewsHeader>
-                    <div className="telepules_uzemeltetes_img">
-                        <img
-                            src="/images/middle_content_pics/telepules_uzemeltetes_pics/2015-06-30_tranzit_d_big.jpg"
-                            alt="Európai Unió támogatásával megvalósuló projekt táblája 89,85 millió forint támogatásról."
-                        />
+                    <div className="telepules_uzemeltetes_news">
+                        <NewsHeader new_date="2015. június 30.">
+                            Tranzitfoglalkoztatás megvalósítása Cigánd városában
+                        </NewsHeader>
+                        <div className="telepules_uzemeltetes_img">
+                            <img
+                                src="/images/middle_content_pics/telepules_uzemeltetes_pics/2015-06-30_tranzit_d_big.jpg"
+                                alt="Európai Unió támogatásával megvalósuló projekt táblája 89,85 millió forint támogatásról."
+                            />
+                        </div>
+                        <a
+                            href="images/middle_content_pics/telepules_uzemeltetes_pics/2015-06-30_tranzit_d_big.jpg"
+                            target="_blank"
+                            className="align-center bold display-block border_tu_nagyitas"
+                        >
+                            Nagyítás
+                        </a>
                     </div>
-                    <a
-                        href="images/middle_content_pics/telepules_uzemeltetes_pics/2015-06-30_tranzit_d_big.jpg"
-                        target="_blank"
-                        className="align-center bold display-block border_tu_nagyitas"
-                    >
-                        Nagyítás
-                    </a>
+
+                    <div className="telepules_uzemeltetes_news">
+                        <NewsHeader new_date="2015. június 28.">
+                            Sajtóközlemény
+                        </NewsHeader>
+                        <p>
+                            Befejeződött a Cigánd Településüzemeltetési és
+                            Városfejlesztő Nonprofit Közhasznú Kft., Cigánd
+                            Város Önkormányzata és a Cigánd Városi Roma
+                            Nemzetiségi Önkormányzat közös pályázata.
+                        </p>
+                        <p>
+                            A{" "}
+                            <span className="bold">
+                                Tranzitfoglalkoztatás megvalósítása Cigánd
+                                városában
+                            </span>{" "}
+                            című TÁMOP-1.4.6-13/1-2013-0046 jelű projekt
+                            eredményei
+                        </p>
+                        <ul className="telepules_uzemeltetes_ul">
+                            <li>20 fő projektbe való bevonása</li>
+                            <li>
+                                18 fő felkészítése, képzése és foglalkoztatása 6
+                                hónapon keresztül
+                            </li>
+                            <li>3 fő mentor alkalmazása</li>
+                            <li>
+                                a képzéssel egybekötött foglalkoztatás
+                                megkezdéséhez szükséges technikai eszközök
+                                beszerzése:
+                            </li>
+                        </ul>
+                        <p>
+                            1 db fekvő horizont fűrész, ipari, 1 db betonkeverő,
+                            elektromos ipari, 0,5kW, 2 db festékkeverő gép, 1 db
+                            homlokzati állvány 400m2, fixállású, 3 db
+                            láncfűrész+tartozék, benzinmotoros, 1 db elektromos
+                            térkővágó, 7 db henger szett és kiegészítői
+                            (többféle méretben), 7 db ecset és kefe szett
+                            (többféle méretben), 7 db glettelő és falsimítólap
+                            szett (többféle méretben), 7 db erősített falétra,
+                            vödörakasztóval, 5 db térkőgyártó sablon (különböző
+                            forma)
+                        </p>
+                        <p>
+                            2015. május 19-én 5 fő zsaluzóács, 6 fő festő, 6 fő
+                            kőműves OKJ végzettséget szerzett. A megszerzett
+                            szakmájukkal 3 hónapig tovább dolgoznak a Cigánd
+                            Településüzemeltetési és Városfejlesztő NKft-nél
+                            majd ezután remélhetőleg az új szakmájukkal
+                            sikeresen el tudnak helyezkedni a munkaerőpiacon.
+                        </p>
+
+                        <p className="bold">
+                            Projekt időtartama 2013.12.27- 2015.06.30.
+                        </p>
+
+                        <p className="bold">A támogatás mértéke 100%.</p>
+
+                        <p className="bold">A támogatás összege 89,95 MFt</p>
+
+                        <p className="bold">Cigánd, Értékből Lendület!</p>
+                    </div>
+
+                    <div className="telepules_uzemeltetes_news">
+                        <NewsHeader new_date="2015. május 21.">
+                            Sajtóközlemény
+                        </NewsHeader>
+                        <p>
+                            A Cigánd Településüzemeltetési és Városfejlesztő
+                            Nonprofit Közhasznú Kft., Cigánd Város Önkormányzata
+                            és a Cigánd Városi Roma Nemzetiségi Önkormányzat
+                            közösen és sikeresen pályázott 2013. június 27-én a
+                            TÁMOP keretében meghirdetett „Tranzitfoglalkoztatás
+                            az építőiparban” című felhívásra.
+                        </p>
+                        <p>
+                            A{" "}
+                            <span className="bold">
+                                Tranzitfoglalkoztatás megvalósítása Cigánd
+                                városában
+                            </span>{" "}
+                            című TÁMOP-1.4.6-13/1-2013-0046 jelű projekt eddigi
+                            eredményei
+                        </p>
+                        <ul>
+                            <li>20 fő projektbe való bevonása</li>
+                            <li>
+                                18 fő felkészítése, képzése és foglalkoztatása 6
+                                hónapon keresztül
+                            </li>
+                            <li>3 fő mentor alkalmazása</li>
+                            <li>
+                                a képzéssel egybekötött foglalkoztatás
+                                megkezdéséhez szükséges technikai eszközök
+                                beszerzése:
+                            </li>
+                        </ul>
+                        <p>
+                            1 db fekvő horizont fűrész, ipari, 1 db betonkeverő,
+                            elektromos ipari, 0,5kW, 2 db festékkeverő gép, 1 db
+                            homlokzati állvány 400m2, fixállású, 3 db
+                            láncfűrész+tartozék, benzinmotoros, 1 db elektromos
+                            térkővágó, 7 db henger szett és kiegészítői
+                            (többféle méretben), 7 db ecset és kefe szett
+                            (többféle méretben), 7 db glettelő és falsimítólap
+                            szett (többféle méretben), 7 db erősített falétra,
+                            vödörakasztóval, 5 db térkőgyártó sablon (különböző
+                            forma)
+                        </p>
+                        <p>
+                            2015. május 19-én 5 fő zsaluzóács, 6 fő festő, 6 fő
+                            kőműves OKJ végzettséget szerzett. A megszerzett
+                            szakmájukkal 3 hónapig tovább dolgoznak a Cigánd
+                            Településüzemeltetési és Városfejlesztő NKft-nél
+                            majd ezután remélhetőleg az új szakmájukkal
+                            sikeresen el tudnak helyezkedni a munkaerőpiacon.
+                        </p>
+
+                        <p className="bold">
+                            Projekt időtartama 2013.12.27- 2015.06.30.
+                        </p>
+
+                        <p className="bold">A támogatás mértéke 100%.</p>
+
+                        <p className="bold">A támogatás összege 89,95 MFt</p>
+                    </div>
+
+                    <div className="telepules_uzemeltetes_news">
+                        <NewsHeader new_date="2015. május 18.">
+                            Vizsga
+                        </NewsHeader>
+                        <div className="telepules_uzemeltetes_img">
+                            <img
+                                src="/images/middle_content_pics/telepules_uzemeltetes_pics/2015-05-18_vizsga.jpg"
+                                alt="Szakmunkások vizsgája a Cigánd Településüzemeltetési és Városfejlesztő NKft-nél építőipari projektben."
+                            />
+                        </div>
+                        <a
+                            href="images/middle_content_pics/telepules_uzemeltetes_pics/2015-05-18_vizsga.jpg"
+                            target="_blank"
+                            className="align-center bold display-block border_tu_nagyitas"
+                        >
+                            Nagyítás
+                        </a>
+                    </div>
+
+                    <div className="telepules_uzemeltetes_news">
+                        <NewsHeader new_date="2014. július 14.">
+                            Eszközök
+                        </NewsHeader>
+                        <div className="telepules_uzemeltetes_img">
+                            <img
+                                src="/images/middle_content_pics/telepules_uzemeltetes_pics/2014-07-14_eszkozok.jpg"
+                                alt="Elnyert építőipari munkákhoz szükséges eszközök."
+                            />
+                        </div>
+                        <a
+                            href="images/middle_content_pics/telepules_uzemeltetes_pics/2014-07-14_eszkozok.jpg"
+                            target="_blank"
+                            className="align-center bold display-block border_tu_nagyitas"
+                        >
+                            Nagyítás
+                        </a>
+                    </div>
                 </div>
             </ContentContainer>
         </>
